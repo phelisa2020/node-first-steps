9abbd84d8ff619a745d994adf278f95604f696dc
phelisa2020 <phelisagqwetha@gmail.com>
2020-08-04 14:47:08 +0200

    my commit

--- greet_xola.js
+++ greet_xola.js
@@ -6,16 +6,18 @@ const chalk = require('chalk');
 //import the greet module that is in the current folder
 const greet = require('./greet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+
 
 var figlet = require('figlet');
  
-figlet('Hello Xola!!', function(err, data) {
+figlet(greet('Xola'), function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(data)
+    //console.log(data)
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage)
+
 });
