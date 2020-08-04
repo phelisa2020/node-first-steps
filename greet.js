7d6bff0c753df2997d3c50c6f3a957715c0e819c
phelisa2020 <phelisagqwetha@gmail.com>
2020-08-04 12:39:28 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,11 @@
+// function greet(name) {
+//   var result ="Hello, "+name 
+  
+// return result 
+// };
+
+// var msg = greet('Xola')
+// console.log(msg)
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
